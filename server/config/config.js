54557cb993cb8c4b7b4218843789fc96d9e3ea73
Wilmer Lopez gil <wilmerlopezgil@MacBook-Pro-de-Wilmer-2.local>
2018-07-08 21:33:17 -0500

    variable de entorno heroku ocultar conexion db

--- server/config/config.js
+++ server/config/config.js
@@ -23,7 +23,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ){
   urlDB = 'mongodb://localhost:27017/cafe'
 }else {
-  urlDB = 'mongodb://cafe-user:cafe-user123@ds129831.mlab.com:29831/cafe_'
+  urlDB = process.env.MONGO_URI;
 }
 
 
